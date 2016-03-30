65120b9a8c46648153fe26c6e56dd50b7e8e343a
lowell <lowell@lowelldeMacBook-Pro.local>
2016-03-30 14:44:43 +0800

    branch commit test

---
+++ YDKit/YDKit/YDKit.h
@@ -0,0 +1,21 @@
+//
+//  YDKit.h
+//  YDKit
+//
+//  Created by lowell on 3/30/16.
+//  Copyright © 2016 Y&D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YDKit.
+FOUNDATION_EXPORT double YDKitVersionNumber;
+
+//! Project version string for YDKit.
+FOUNDATION_EXPORT const unsigned char YDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YDKit/PublicHeader.h>
+
+#import "AFNetworking.h"
+
+#import "FirstTest.h"

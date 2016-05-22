685052461416d5251b6b558eeb84e9cd819d6533
lowell <545333574@qq.com>
2016-05-22 14:26:26 +0800

    ggggg

--- YBFreight/YBFreight/Modules/YBPersonalCenter/ViewControllers/YBPersonalCenterVC.m
+++ YBFreight/YBFreight/Modules/YBPersonalCenter/ViewControllers/YBPersonalCenterVC.m
@@ -10,6 +10,13 @@
 
 @interface YBPersonalCenterVC ()
 
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIView *tableHeaderView;
+@property (nonatomic, strong) UIButton *avatarBtn;
+
+- (UITableView *)tableView;
+- (UIView *)tableHeaderView;
+
 @end
 
 @implementation YBPersonalCenterVC
@@ -17,22 +24,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self initUI];
+}
+
+- (void)initUI {
     self.title = @"我的";
+    
+    [self.view addSubview:self.tableView];
+}
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.tableHeaderView = self.tableHeaderView;
+    }
+    return _tableView;
+}
+
+- (UIView *)tableHeaderView {
+    if (_tableHeaderView) {
+        _tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.tableView.bounds), 100)];
+        
+        UIButton *avatarBtn;
+//        [avatarBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+//            
+//        }];
+//        [avatarBtn sd_setImageWithURL:[NSURL new] forState:UIControlStateNormal placeholderImage:nil];
+    }
+    return _tableHeaderView;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
